afeca75793a09f88e389362695f66d0e01adb9f0
Pieter Claerhout <pieter@yellowduck.be>
2019-10-30 11:26:49 +0100

    Initial commit

--- cmd/example-grpc/main.go
+++ cmd/example-grpc/main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"fmt"
 
+	examplegrpc "github.com/pieterclaerhout/example-grpc"
 	"github.com/pieterclaerhout/example-grpc/versioninfo"
+	"github.com/pieterclaerhout/go-log"
 )
 
 func main() {
-	fmt.Println("Project: "+ versioninfo.ProjectName)
-	fmt.Println("Description: "+ versioninfo.ProjectDescription)
-	fmt.Println("Copyright: "+ versioninfo.ProjectCopyright)
-	fmt.Println("Version: "+ versioninfo.Version)
+	fmt.Println("Project: " + versioninfo.ProjectName)
+	fmt.Println("Description: " + versioninfo.ProjectDescription)
+	fmt.Println("Copyright: " + versioninfo.ProjectCopyright)
+	fmt.Println("Version: " + versioninfo.Version)
 	fmt.Println("Revision: " + versioninfo.Revision)
 	fmt.Println("Branch: " + versioninfo.Branch)
+
+	g := examplegrpc.New()
+	err := g.Start()
+	log.CheckError(err)
+
 }
--- library.go
+++ library.go
@@ -1,1 +1,37 @@
 package examplegrpc
+
+import (
+	context "context"
+	fmt "fmt"
+	"net"
+
+	grpc "google.golang.org/grpc"
+)
+
+// Greeter ...
+type Greeter struct {
+}
+
+// New creates new server greeter
+func New() *Greeter {
+	return &Greeter{}
+}
+
+// Start starts server
+func (g *Greeter) Start() error {
+	lis, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		return err
+	}
+	grpcServer := grpc.NewServer()
+	RegisterGreeterServer(grpcServer, g)
+	grpcServer.Serve(lis)
+	return nil
+}
+
+// SayHello says hello
+func (g *Greeter) SayHello(ctx context.Context, r *HelloRequest) (*HelloReply, error) {
+	return &HelloReply{
+		Message: fmt.Sprintf("Hello, %s!", r.Name),
+	}, nil
+}
