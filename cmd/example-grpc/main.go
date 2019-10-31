6605b61a803ca4bbe728641c39fe79cbe830268f
Pieter Claerhout <pieter@yellowduck.be>
2019-10-31 13:01:32 +0100

    Added a client

--- cmd/example-grpc/main.go
+++ cmd/example-grpc/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	examplegrpc "github.com/pieterclaerhout/example-grpc"
 	"github.com/pieterclaerhout/example-grpc/versioninfo"
 	"github.com/pieterclaerhout/go-log"
+	"google.golang.org/grpc"
 )
 
 func main() {
+
 	fmt.Println("Project: " + versioninfo.ProjectName)
 	fmt.Println("Description: " + versioninfo.ProjectDescription)
 	fmt.Println("Copyright: " + versioninfo.ProjectCopyright)
@@ -16,8 +19,27 @@ func main() {
 	fmt.Println("Revision: " + versioninfo.Revision)
 	fmt.Println("Branch: " + versioninfo.Branch)
 
-	g := examplegrpc.New()
-	err := g.Start()
+	go func() {
+
+		log.Info("Starting gRPC server")
+
+		g := examplegrpc.New()
+		err := g.Start()
+		log.CheckError(err)
+
+	}()
+
+	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
 	log.CheckError(err)
+	defer conn.Close()
+
+	client := examplegrpc.NewGreeterClient(conn)
+
+	result, err := client.SayHello(context.Background(), &examplegrpc.HelloRequest{
+		Name: "Pieter",
+	})
+	log.CheckError(err)
+
+	log.WarnDump(result.GetMessage(), "result")
 
 }
